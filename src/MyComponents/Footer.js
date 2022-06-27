d44ccf223c23fff0fac0568d8c95d0d1aa92c562
Saurav Kumar <sauravkumar843114@gmail.com>
2022-06-28 01:27:30 +0530

    Code

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light py-3">
+        <p className='text-center'>
+            Copyright &copy; MyTodosList.com
+        </p>
+      
+    </footer>
+  )
+}
+
+export default Footer
